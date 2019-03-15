f024565c6e5638085cf81ebdf2223c005320e3c2
Anne <anne.jurack@gmail.com>
2019-03-15 11:43:39 -0500

    Added gitognore and project files

---
+++ MyProject/Models/MyModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+namespace MyProject.Models
+{
+    public class MyModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
